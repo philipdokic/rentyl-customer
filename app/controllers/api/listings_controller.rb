edf81d72823fb534bd2913a742f625e4fc81376a
starjdave <strcdavid@gmail.com>
2020-10-14 08:37:26 -0500

    Remove jbuilder view for individual listing

--- app/controllers/api/listings_controller.rb
+++ app/controllers/api/listings_controller.rb
@@ -25,6 +25,23 @@ class Api::ListingsController < ApplicationController
   # SHOW -----------------------------------------
   # ----------------------------------------------
   def show
+    render json: {
+      id: @listing.id,
+      currency: @listing.currency,
+      unit: @listing.unit,
+      property: @listing.unit.property,
+      property_manager: @listing.unit.property.get_manager,
+      location: @listing.unit.property.location,
+      bedrooms: @listing.unit.bedrooms,
+      bathrooms: @listing.unit.bathrooms,
+      reviews: @listing.unit.reviews.with_status("published").order('reviewed_date DESC'),
+      review_average: @listing.unit.reviews.with_status("published").average(:rating),
+      availability: @listing.unit.unit_availability,
+      availability_calendar: @listing.unit.unit_availability.cx_availability_calendar,
+      booking_calendar: @listing.unit.unit_availability.booking_calendar,
+      default_availability_changeover: @listing.unit.unit_availability.default_availability_changeover,
+      average_default_nightly_price: @listing.unit_pricing.average_default_nightly_price
+    }
   end
 
   # ==============================================
