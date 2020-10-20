99d83d18fb26d5ca08d5ae35a24fe7083ff180f1
starjdave <strcdavid@gmail.com>
2020-10-20 17:14:46 -0500

    Eager load the included unit listing models

--- app/controllers/api/listings_controller.rb
+++ app/controllers/api/listings_controller.rb
@@ -6,7 +6,7 @@ class Api::ListingsController < ApplicationController
   # ----------------------------------------------
   # CALLBACKS ------------------------------------
   # ----------------------------------------------
-  before_action :set_listing, only: [:show]
+  #before_action :set_listing, only: [:show]
 
   # ==============================================
   # ACTIONS ======================================
@@ -25,21 +25,58 @@ class Api::ListingsController < ApplicationController
   # SHOW -----------------------------------------
   # ----------------------------------------------
   def show
+    # listing = ActiveRecord::Base.connection.execute("
+    #   SELECT
+    #     unit_listings.id,
+    #     unit_listings.currency,
+    #     unit_listings.is_multi_unit,
+    #     unit_listings.is_room_type,
+    #     units.id as unit_id,
+    #     bedrooms.*
+    #   FROM unit_listings
+    #   INNER JOIN units
+    #     ON unit_listings.unit_id = units.id
+    #   INNER JOIN properties
+    #     ON units.property_id = properties.id
+    #   INNER JOIN bedrooms
+    #     ON bedrooms.unit_id = bedrooms.id
+    #   WHERE slug = 'belmont-chalet'
+    # ").first
+
+
+    test = UnitListing.where(slug: 'belmont-chalet').includes({ property: [:location] }, { unit: [:bathrooms, :bedrooms, :reviews] }).first
     render json: {
-      id: @listing.id,
-      currency: @listing.currency,
-      multi_unit: @listing.is_multi_unit,
-      room_type: @listing.is_room_type,
-      unit: @listing.unit,
-      property: @listing.unit.property,
-      property_manager: @listing.unit.property.get_manager,
-      location: @listing.unit.property.location,
-      bedrooms: @listing.unit.bedrooms,
-      bathrooms: @listing.unit.bathrooms,
-      reviews: @listing.unit.reviews.with_status("published").order('reviewed_date DESC'),
-      review_average: @listing.unit.reviews.with_status("published").average(:rating),
+      id: test.id,
+      currency: test.currency,
+      multi_unit: test.is_multi_unit,
+      room_type: test.is_room_type,
+      unit: test.unit,
+      property: test.unit.property,
+      property_manager: test.unit.property.get_manager,
+      location: test.unit.property.location,
+      bedrooms: test.unit.bedrooms,
+      bathrooms: test.unit.bathrooms,
+      reviews: test.unit.reviews.with_status("published").order('reviewed_date DESC'),
+      review_average: test.unit.reviews.with_status("published").average(:rating),
       google_maps_api_key: ENV['GOOGLE_MAPS_API_KEY']
     }
+
+
+    # render json: {
+    #   id: @listing.id,
+    #   currency: @listing.currency,
+    #   multi_unit: @listing.is_multi_unit,
+    #   room_type: @listing.is_room_type,
+    #   unit: @listing.unit,
+    #   property: @listing.unit.property,
+    #   property_manager: @listing.unit.property.get_manager,
+    #   location: @listing.unit.property.location,
+    #   bedrooms: @listing.unit.bedrooms,
+    #   bathrooms: @listing.unit.bathrooms,
+    #   reviews: @listing.unit.reviews.with_status("published").order('reviewed_date DESC'),
+    #   review_average: @listing.unit.reviews.with_status("published").average(:rating),
+    #   google_maps_api_key: ENV['GOOGLE_MAPS_API_KEY']
+    # }
   end
 
   # ----------------------------------------------
