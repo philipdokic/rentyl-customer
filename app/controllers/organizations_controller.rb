01cff50730d0a32aee3c917ce9130ef001e8f5db
starjdave <strcdavid@gmail.com>
2021-04-26 14:02:52 -0500

    Add puts

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -11,12 +11,17 @@ class OrganizationsController < ApplicationController
   # CATCH-ALL-REDIRECT ---------------------------
   # ----------------------------------------------
   def catch_all_redirect
+    puts "HIT"
+    puts request.host
+    puts request.fullpath
     legacy_redirect = LegacyRedirect.from_host_and_path(
       request.host,
       request.fullpath
     )
 
     if legacy_redirect.blank?
+      puts "BLANK"
+      puts legacy_redirect.inspect
       redirect_to '/'
       return
     end
