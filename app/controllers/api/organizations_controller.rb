ef77610ab8c52437f1724ca20db922c683757530
starjdave <strcdavid@gmail.com>
2021-04-27 17:33:56 -0500

    Don't load featured pages if not enabled

--- app/controllers/api/organizations_controller.rb
+++ app/controllers/api/organizations_controller.rb
@@ -57,39 +57,43 @@ class Api::OrganizationsController < ApplicationController
   # ----------------------------------------------
   # TODO: refactor
   def build_featured_listing_models
-    featured_listings = @brand.unit_listings.where(featured: true)
-    featured_listings_info = []
-    featured_listings.each do |l|
-      l_obj = {}
-      l_obj[:id] = l.id
-      l_obj[:data] = l
-      l_obj[:unit_name] = l.unit.name
-      l_obj[:unit_type] = l.unit.unit_type
-      l_obj[:num_units] = l.property.units.length
-      l_obj[:property_name] = l.property.name
-      l_obj[:property_type] = l.property.property_type
-      l_obj[:location] = l.property.location
-      l_obj[:guests] = l.unit.num_sleep
-      l_obj[:slug] = l.slug
-      l_obj[:multi_unit] = l.property.is_multi_unit?
-      l_obj[:bathrooms] = l.unit.num_bathrooms
-      l_obj[:bedrooms] = l.unit.num_bedrooms
-      if(l.property.is_multi_unit?)
-        if(l.unit.unit_images.length)
-          l_obj[:image] = l.unit.featured_image
-        elsif(l.property.property_images.length)
-          l_obj[:image] = l.property.property_images.first
+    if @brand.brand_home_page.options["show_featured_properties"] == "true"
+      featured_listings = @brand.unit_listings.where(featured: true)
+      featured_listings_info = []
+      featured_listings.each do |l|
+        l_obj = {}
+        l_obj[:id] = l.id
+        l_obj[:data] = l
+        l_obj[:unit_name] = l.unit.name
+        l_obj[:unit_type] = l.unit.unit_type
+        l_obj[:num_units] = l.property.units.length
+        l_obj[:property_name] = l.property.name
+        l_obj[:property_type] = l.property.property_type
+        l_obj[:location] = l.property.location
+        l_obj[:guests] = l.unit.num_sleep
+        l_obj[:slug] = l.slug
+        l_obj[:multi_unit] = l.property.is_multi_unit?
+        l_obj[:bathrooms] = l.unit.num_bathrooms
+        l_obj[:bedrooms] = l.unit.num_bedrooms
+        if(l.property.is_multi_unit?)
+          if(l.unit.unit_images.length)
+            l_obj[:image] = l.unit.featured_image
+          elsif(l.property.property_images.length)
+            l_obj[:image] = l.property.property_images.first
+          end
+        else
+          if(l.property.property_images.length)
+            l_obj[:image] = l.property.property_images.first
+          elsif(l.unit.unit_images.length)
+            l_obj[:image] = l.unit.featured_image
+          end
         end
-      else
-        if(l.property.property_images.length)
-          l_obj[:image] = l.property.property_images.first
-        elsif(l.unit.unit_images.length)
-          l_obj[:image] = l.unit.featured_image
-        end
+        featured_listings_info.push(l_obj)
       end
-      featured_listings_info.push(l_obj)
+      @featured_listings = featured_listings_info
+    else
+      @featured_listings = nil
     end
-    @featured_listings = featured_listings_info
   end
 
   # ----------------------------------------------
