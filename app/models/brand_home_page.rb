727bef9e751b7a51ca652f010eb36a96f7347936
starjdave <strcdavid@gmail.com>
2021-05-07 12:36:16 -0500

    Home page header

--- app/models/brand_home_page.rb
+++ app/models/brand_home_page.rb
@@ -1,5 +1,5 @@
 class BrandHomePage < ApplicationRecord
-    connects_to database: { writing: :direct, reading: :direct_replica }
+  connects_to database: { writing: :direct, reading: :direct_replica }
   belongs_to :brand
   has_one :hero_image, as: :hero_imageable
 end
