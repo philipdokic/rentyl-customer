dd72e7e8628c74514511b74cf53caa079a3aa436
starjdave <strcdavid@gmail.com>
2021-05-12 11:45:13 -0500

    Revert num guests

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -120,7 +120,7 @@ class Api::BookingsController < ApplicationController
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = (uri.scheme == "https")
       request = Net::HTTP::Get.new(uri.path, {'Content-Type' => 'application/json'})
-      request.body = {check_in: Date.parse(params['check_in']), check_out: Date.parse(params['check_out']), num_guests: params['num_guests'].to_i}.to_json # , num_guests: params['num_guests'].to_i
+      request.body = {check_in: Date.parse(params['check_in']), check_out: Date.parse(params['check_out'])}.to_json # , num_guests: params['num_guests'].to_i
       @quote ||= JSON.parse(http.request(request).body)
     end
   end
